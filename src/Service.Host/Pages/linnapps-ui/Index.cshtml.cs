a7b6c8769e21c0b685ebceae671296793dab0428
richard phillips <richard.phillips@linn.co.uk>
2018-08-20 16:56:49 +0100

    separate logging and proxy

--- src/Service.Host/Pages/linnapps-ui/Index.cshtml.cs
+++ src/Service.Host/Pages/linnapps-ui/Index.cshtml.cs
@@ -1,7 +1,6 @@
 ﻿namespace Linn.LinnappsUi.Service.Host.Pages
 {
     using Linn.Common.Logging;
-    using Linn.LinnappsUi.Domain.RemoteServices;
 
     using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,21 +8,14 @@
     {
         private readonly ILog log;
 
-        private readonly IThingService thingService;
-
-        public IndexModel2(ILog log, IThingService thingService)
+        public IndexModel2(ILog log)
         {
             this.log = log;
-            this.thingService = thingService;
         }
-
-        public string DisplayThing { get; set; }
 
         public void OnGet()
         {
             this.log.Debug("Looked at index");
-
-            this.DisplayThing = this.thingService.GetThing();
         }
     }
 }
