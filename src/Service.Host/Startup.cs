9e5d6c945936e96d1d1fb005eb7170ea2583c86a
richard phillips <richard.phillips@linn.co.uk>
2018-08-20 11:28:01 +0100

    add ioc

--- src/Service.Host/Startup.cs
+++ src/Service.Host/Startup.cs
@@ -1,6 +1,12 @@
 namespace Linn.LinnappsUi.Service.Host
 {
+    using System;
+
+    using Autofac;
+    using Autofac.Extensions.DependencyInjection;
+
     using Linn.Common.Configuration;
+    using Linn.LinnappsUi.IoC;
     using Linn.LinnappsUi.Persistence;
 
     using Microsoft.AspNetCore.Builder;
@@ -10,6 +16,7 @@ namespace Linn.LinnappsUi.Service.Host
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
 
     public class Startup
     {
@@ -21,7 +28,7 @@ namespace Linn.LinnappsUi.Service.Host
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
@@ -33,11 +40,22 @@ namespace Linn.LinnappsUi.Service.Host
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<ServiceDbContext>(options => options.UseOracle(this.GetConnectionString()));
+
+            // Add Autofac
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule<AmazonCredentialsModule>();
+            containerBuilder.RegisterModule<AmazonSqsModule>();
+            containerBuilder.RegisterModule<LoggingModule>();
+            containerBuilder.Populate(services);
+            var container = containerBuilder.Build();
+            return new AutofacServiceProvider(container);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
