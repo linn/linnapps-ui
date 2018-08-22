ca7297852604a76b4ff68057585693310b7ba6d3
richard phillips <richard.phillips@linn.co.uk>
2018-08-22 09:54:28 +0100

    run sql report query

---
+++ src/Service.Host/Pages/linnapps-ui/Products/SaCoreTypes/CoreTypeBySalesArticle.cshtml.cs
@@ -0,0 +1,26 @@
+﻿namespace Linn.LinnappsUi.Service.Host.Pages.Products.SaCoreTypes
+{
+    using System.Collections.Generic;
+
+    using Linn.LinnappsUi.Domain.ReportModels;
+    using Linn.LinnappsUi.Domain.Reports;
+
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class CoreTypeBySalesArticleModel : PageModel
+    {
+        private readonly ISaCoreTypesReportService reportService;
+
+        public CoreTypeBySalesArticleModel(ISaCoreTypesReportService reportService)
+        {
+            this.reportService = reportService;
+        }
+
+        public IEnumerable<SalesArticleCoreType> SalesArticleCoreTypes { get; set; }
+
+        public void OnGet()
+        {
+            this.SalesArticleCoreTypes = this.reportService.GetCoreTypesBySalesArticle();
+        }
+    }
+}
