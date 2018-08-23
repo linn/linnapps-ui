cd86f535d93b6089785de4a5db5e824bd989e61a
Richard Phillips <richardiphillips@users.noreply.github.com>
2018-08-23 14:18:18 +0100

    add csv download option (#6)
    * csv download
    
    * change deployment of prod

---
+++ src/Domain/Reports/Helpers/CsvCreator.cs
@@ -0,0 +1,18 @@
+﻿namespace Linn.LinnappsUi.Domain.Reports.Helpers
+{
+    using Linn.Common.Serialization;
+
+    public class CsvCreator : ICsvCreator
+    {
+        public byte[] CreateCsv(object data)
+        {
+            var outputStream = new System.IO.MemoryStream();
+            using (var writer = new CsvStreamWriter(outputStream))
+            {
+                writer.WriteModel(data);
+            }
+
+            return outputStream.ToArray();
+        }
+    }
+}
