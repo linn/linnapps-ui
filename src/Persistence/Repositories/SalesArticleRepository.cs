5edf6cd922eabdc63c0092401d4e6f507eb32478
richard phillips <richard.phillips@linn.co.uk>
2018-08-30 10:12:05 +0100

    try simpler search for speed comparison

--- src/Persistence/Repositories/SalesArticleRepository.cs
+++ src/Persistence/Repositories/SalesArticleRepository.cs
@@ -30,10 +30,12 @@
             var splits = searchTerm.Split(' ');
             var articles = new List<SalesArticle>();
 
-            articles.AddRange(this.serviceDbContext.SalesArticle
-                .Where(s => s.ArticleNumber.Contains(searchTerm)));
-            articles.AddRange(this.serviceDbContext.SalesArticle
-                .Where(s => !s.ArticleNumber.Contains(searchTerm) && this.Matches(s.ArticleNumber, s.InvoiceDescription, splits)));
+            // articles.AddRange(this.serviceDbContext.SalesArticle
+            //     .Where(s => s.ArticleNumber.Contains(searchTerm)));
+            // articles.AddRange(this.serviceDbContext.SalesArticle
+            //     .Where(s => !s.ArticleNumber.Contains(searchTerm) && this.Matches(s.ArticleNumber, s.InvoiceDescription, splits)));
+            return this.serviceDbContext.SalesArticle
+                .Where(s => this.Matches(s.ArticleNumber, s.InvoiceDescription, splits));
 
             return articles;
         }
